7e163ca50c31af3cbfbe248fa88202aa5fade7b8
Georgia Tardy <georgiatardyart@gmail.com>
2017-08-30 16:42:20 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Styles/index.css';
+import App from './App';
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
